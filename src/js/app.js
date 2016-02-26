003f81f8940974b04ea4678661859888b3e34af4
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-02-26 16:07:42 +0100

    easter eggs not loaded on mobile

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,42 @@ var Draggable = require('./vendors/Draggable.js');
 // https://github.com/samthor/rippleJS, not a commonJS module
 require('./vendors/ripple.js');
 
+function assignEasterEggs(){
+  var links = {
+    'ee_balance-bear.gif': 'balanceBear',
+    'ee_client.gif': 'clientReaction',
+    'ee_gianluca.png': 'gianlucaMeme',
+    'ee_pizza.gif': 'pizzaGradient',
+    'ee_psycho-cat.gif ': 'psychoCat',
+    'ee_pushforce.png': 'pushForce',
+    'ee_too-quick-devops.gif': 'tooQuickDevops',
+    'ee_ux-glass.gif': 'uxGlass',
+    'ee_vvo.jpg': 'vvoThumb'  
+  };
+  for (var key in links) {
+    if (links.hasOwnProperty(key)) {
+      document.getElementById(links[key]).src = 'img/'+key
+    }
+  }
+}
+
+
+
 function isMobile(){
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    
+    return;
   }
 
-  else {
-    var droppableArr = [];
-    // initialize draggable(s)
-    $( '.draggable' ).map( function(i) {
-      new Draggable( this, droppableArr, {
-        draggabilly : { containment: document.body }
-      } );
+  var droppableArr = [];
+  // initialize draggable(s)
+  $( '.draggable' ).map( function(i) {
+    new Draggable( this, droppableArr, {
+      draggabilly : { containment: document.body }
     } );
-  }
+  } );
+
+  assignEasterEggs()
+
 }
 
 window.addEventListener('load', function(){
