a18dea8f066d72f530c30248fc25e8266d049c01
Jonas <jonas.badalic@algolia.com>
2017-02-13 10:21:07 +0100

    remove id's test

--- gulpfile.js
+++ gulpfile.js
@@ -81,10 +81,7 @@ gulp.task('icons', function () {
     var prefix = path.basename(file.relative, path.extname(file.relative));
     return {
       plugins: [{
-        cleanupIDs: {
-          prefix: prefix + '-',
-          minify: true
-        }
+        cleanupIDs: false
       }]
     }
   }))
@@ -184,7 +181,10 @@ gulp.task('images:optim', function () {
   return gulp.src('src/img/**/*')
     .pipe(imagemin({
       progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
+      svgoPlugins: [
+        {removeViewBox: false},
+        {cleanupIDs: false}
+        ],
       use: [pngquant()]
     }))
     .pipe(gulp.dest('build/img'));
--- src/js/app.js
+++ src/js/app.js
@@ -127,7 +127,6 @@ const renderCategoryTemplate = (category) => {
 }
 
 const renderItem = (data) => {
-  console.log(data);
   const wrapperDiv = document.createElement("div");
   wrapperDiv.className = "ais-hits--item";
   wrapperDiv.innerHTML = `
