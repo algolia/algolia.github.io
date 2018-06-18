2f9bd20044bd68b7c98eb7ebbfc30b868b89c2f9
vvo <vincent.voyer@algolia.com>
2018-06-18 16:11:21 +0200

    fix build step

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,9 @@ const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
 
 // prod
+const filter = require('gulp-filter');
 const rev = require('gulp-rev');
-const revNapkin = require('gulp-rev-napkin');
-const revReplace = require('gulp-rev-replace');
+const revRewrite = require('gulp-rev-rewrite');
 
 const minifyCss = require('gulp-minify-css');
 const uglify = require('gulp-uglify-es').default;
@@ -210,39 +210,13 @@ gulp.task('webserver', () => {
 // -------------------------------------
 //   Task: Revision
 // -------------------------------------
-gulp.task('revision', () =>
-  gulp
-    .src([
-      'build/**/**/*.{css,js,png,svg,html}',
-      '!build/js/serviceWorker.js',
-      '!build/img/*.svg',
-    ])
-    .pipe(rev())
-    .pipe(gulp.dest('build'))
-    .pipe(revNapkin())
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('build'))
-);
-
-gulp.task('revreplace', ['revision'], () => {
-  const manifest = gulp.src('build/rev-manifest.json');
-
-  return gulp
-    .src(
-      [
-        'build/*.html',
-        'build/js/*.js',
-        'build/*.js',
-        'build/*.css',
-        'build/manifest.json',
-      ],
-      { base: 'build' }
-    )
-    .pipe(
-      revReplace({
-        manifest,
-      })
-    )
+gulp.task('rev', () => {
+  const assetFilter = filter(['**/*', '!**/index.html', '!favicon.ico'], {restore: true});
+  return gulp.src('src/**')
+    .pipe(assetFilter)
+    .pipe(rev()) // Rename all files except index.html
+    .pipe(assetFilter.restore)
+    .pipe(revRewrite()) // Substitute in new filenames
     .pipe(gulp.dest('build'));
 });
 
@@ -269,7 +243,7 @@ gulp.task('build:prod', ['clean'], callback => {
     'images:optim',
     'haml',
     'js:min',
-    'revreplace',
+    'rev',
     'critical-css',
     'copyRedirects',
     'copyWorker',
